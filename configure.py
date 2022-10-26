eec5c8a69c8ff78f909d5d06050db08465572289
PROclimer <116734993+PROclimer@users.noreply.github.com>
2022-10-26 16:57:15 +0300

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 config = {
-	'name': '',
-	'token': '',
-	'tokenqiwi': '',
-	'phoneqiwi': ''
+	'name': 'RitMag_bot',
+	'token': '5345411023:AAEqUEPnlrQFW6SIj7KpkMAD5AWgheJhItg',
+	'tokenqiwi': '1b49031ea16d01ab995cdfa883272aaf',
+	'phoneqiwi': '+79167232172'
 }
